9f12f4fb9d8dc7905dc0ce130b5b552ddb376a94
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-10-11 08:50:44 +0200

    in progress

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
  let data
-
  const add = function() {
-
      let name = document.getElementById("name")
      let nameList = name.value 
      listOfNames(nameList)
@@ -10,13 +8,9 @@
     const listOfNames = function(name) {
         let listOfNames = document.getElementById("listOfNames")  
        let listItem = document.createElement("li") 
+       listItem.classList.add("list-group-item") 
        listItem.innerText = name
        listOfNames.appendChild(listItem)
     }
 
  
-/*
-const add = function() {
-    document.getElementById("listOfNames").innerHTML=document.name.name1.name2;
-
-}*/
